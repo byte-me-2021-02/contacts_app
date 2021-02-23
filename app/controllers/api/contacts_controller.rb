fe2b421a8a901c89128286df7ffed2374d6fecf7
brian <riceb53@gmail.com>
2021-02-22 22:17:42 -0500

    add all contacts route with while loop in view

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,6 +1,13 @@
 class Api::ContactsController < ApplicationController
+  def index
+    # get ALL the contacts
+    @contacts = Contact.all
+    render 'index.json.jb'
+  end
+
   def show
     @contact = Contact.find_by(first_name: "jenny")
     render 'show.json.jb'
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    get "/all_contacts" => "contacts#index"
     get "/contact" => "contacts#show"
   end
 end
